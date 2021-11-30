13e9f3977028d0f71b3312fc53bf91aab59d4c6d
Michael Diramio <56129729+michaeldiramio@users.noreply.github.com>
2021-11-30 17:31:11 +0000

    Initial setup

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Main {
+
+  public static void main(String[] args) {
+    ArrayList<Animal> animals =  new ArrayList<>();
+
+    // add animals here
+    animals.add(new Cat());
+
+
+
+    // make some noise!
+    for(Animal a : animals) {
+      a.makeNoise();
+    }
+
+
+    
+  }
+
+
+}
